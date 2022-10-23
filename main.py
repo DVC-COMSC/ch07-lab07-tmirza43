0f438aca1028cb945cbc571386e9ef686e1e7d60
Talha Mirza <tmirza43@gmail.com>
2022-10-23 15:15:25 -0700

    commit1

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
-inputvalues = input('Enter all elements values: ')
-numbers1 = inputvalues.split() 
-for i in range(len(numbers1)):
-	numbers1[i] = int(numbers1[i]) 
-# The following line is the same as the for-loop
-# numbers1 = list(map(int, numbers))
+numbers = input("Enter 10 int: ").split()
+numbers = list(map(int, numbers))
+print(len(numbers))
 
-# print ("The original list: ", numbers1)
+evenlist = []
 
-# ******************************
-# Make your Code
-# ******************************
+for i in range(5):
+  evenlist.append(numbers.pop(i))
+
+
+print("The list numbers: ", numbers)
+print("The list for even index elements: ", evenlist)
